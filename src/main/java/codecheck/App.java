a550428cb552fbacf12a712bca2eb80a98490623
T.Kishimoto <retkhigh009@gmail.com>
2017-10-15 12:15:24 +0900

    Set outputs in App.java

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -3,8 +3,11 @@ package codecheck;
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+			if (args[i].equals("")) {
+				System.out.println("Hello!");
+			} else {
+				System.out.println("Hello " + args[i] + "!");
+			}
 		}
 	}
 }
